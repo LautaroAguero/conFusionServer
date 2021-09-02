000dcf4b1000bbc4cfa88a0a165fb52bb51b145e
lauta96aguero <lauta96aguero@gmail.com>
2021-09-02 16:24:42 -0300

    "Login-Facebook"

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/confusion",
+  facebook: {
+    clientId: "234284145244982",
+    clientSecret: "75a22283100a2b920df636e8ed8cd731",
+  },
 };
